3c9020f00e832a775e5376c3503dc940114e8532
Guilherme Gouveia Figueiredo Lima <ggfl@cin.ufpe.br>
2019-05-09 11:49:39 -0300

    Só para apresenetação

--- src/MaquinaEstados.v
+++ src/MaquinaEstados.v
@@ -6,6 +6,7 @@ always @(posedge Clock)
     begin
         case (Estado)
             7'b0000001 : Estado <= 7'b0000010;
-            7'b0000010 : Estado <= 7'b0000001;
+            7'b0000010 : Estado <= 7'b0000011;
+            7'b0000011 : Estado <= 7'b0000010;
     end
 endmodule
