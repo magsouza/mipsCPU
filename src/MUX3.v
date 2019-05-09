d218c2c5332b8f120c23f23a1781dff394c953bb
Guilherme Gouveia Figueiredo Lima <ggfl@cin.ufpe.br>
2019-05-09 11:32:56 -0300

    Changing <=

--- src/MUX3.v
+++ src/MUX3.v
@@ -9,13 +9,13 @@ always @(*)
 begin
 	case (muxFlag)
 		2'b00: begin
-			w_muxOut[31:0] <= w_muxIn0[31:0];
+			w_muxOut[31:0] = w_muxIn0[31:0];
 		end
 		2'b01: begin
-			w_muxOut[31:0] <= w_muxIn1[31:0];
+			w_muxOut[31:0] = w_muxIn1[31:0];
 		end
 		2'b10: begin
-			w_muxOut[31:0] <= w_muxIn2[31:0];
+			w_muxOut[31:0] = w_muxIn2[31:0];
 		end
 	endcase
 end
