8827db8ed8809163530284739ddef2670b4154f5
Guilherme Gouveia Figueiredo Lima <ggfl@cin.ufpe.br>
2019-05-09 11:31:54 -0300

    Changing <=

--- src/MUX5.v
+++ src/MUX5.v
@@ -9,19 +9,19 @@ always @(*)
 begin
 	case (muxFlag)
 		3'b000: begin
-			w_muxOut[31:0] <= w_muxIn0[31:0];
+			w_muxOut[31:0] = w_muxIn0[31:0];
 		end
 		3'b001: begin
-			w_muxOut[31:0] <= w_muxIn1[31:0];
+			w_muxOut[31:0] = w_muxIn1[31:0];
 		end
 		3'b010: begin
-			w_muxOut[31:0] <= w_muxIn2[31:0];
+			w_muxOut[31:0] = w_muxIn2[31:0];
 		end
 		3'b011: begin
-			w_muxOut[31:0] <= w_muxIn3[31:0];
+			w_muxOut[31:0] = w_muxIn3[31:0];
         end
         3'b100: begin
-			w_muxOut[31:0] <= w_muxIn4[31:0];
+			w_muxOut[31:0] = w_muxIn4[31:0];
 		end
 	endcase
 end
