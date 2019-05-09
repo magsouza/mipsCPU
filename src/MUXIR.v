3744b83826b199c5e756a263e6ad6f6e00023404
Guilherme Gouveia Figueiredo Lima <ggfl@cin.ufpe.br>
2019-05-09 11:31:09 -0300

    Changing <=

--- src/MUXIR.v
+++ src/MUXIR.v
@@ -9,16 +9,16 @@ always @(*)
 begin
 	case (muxFlag)
 		2'b00: begin
-			w_muxOut[4:0] <= w_muxIn0[4:0];
+			w_muxOut[4:0] = w_muxIn0[4:0];
 		end
 		2'b01: begin
-			w_muxOut[4:0] <= w_muxIn1[4:0];
+			w_muxOut[4:0] = w_muxIn1[4:0];
 		end
 		2'b10: begin
-			w_muxOut[4:0] <= w_muxIn2[4:0];
+			w_muxOut[4:0] = w_muxIn2[4:0];
 		end
 		2'b11: begin
-			w_muxOut[4:0] <= w_muxIn3[4:0];
+			w_muxOut[4:0] = w_muxIn3[4:0];
         	end
 	endcase
 end
